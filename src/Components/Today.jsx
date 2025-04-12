dd5aee5b8535f0bdf3f423528420cf6972f3a3b0
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-04-12 23:07:15 +0530

    today.res

--- src/Components/Today.jsx
+++ src/Components/Today.jsx
@@ -4,7 +4,7 @@ const Today = () => {
   return (
     <div className='container bg-green-900 rounded-xl shadow-2xl border-green-700'>
         <div className='grid grid-cols-1 lg:flex justify-between  px-[5vw] py-[7vh] items-center'>
-            <div className='text-white w-[47vw] '>
+            <div className='text-white w-full lg:w-[47vw] '>
                 <h1 className='text-[35px] font-semibold '>Start your financial journey with <span className='text-red-600'> PMIK804 today!</span></h1>
                 <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur. Blandit odio semper risus pellentesque elit. Pellentesque eget ut imperdiet nulla penatibus. Nascetur viverra arcu sed amet cursus purus.</p>
             </div>
